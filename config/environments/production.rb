d23bbb58c9ddd51b35577833e5ab281da0a1553c
amarpreetjethra <amyjethra@gmail.com>
2016-10-15 20:25:31 +0530

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,14 +9,14 @@ Gyandhan::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
-
+  config.eager_load = true
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-
+  config.log_level = :info
   # Generate digests for assets URLs
   config.assets.digest = true
 
