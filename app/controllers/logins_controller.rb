6cb4c370335a666a6fe9ebf7737e1b3d389a6877
amarpreetjethra <amyjethra@gmail.com>
2016-10-15 19:54:08 +0530

    Version changes to rails 4

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,27 +1,5 @@
 class LoginsController < ApplicationController
   
-
-  # GET /logins/1
-  # GET /logins/1.json
-  def show
-    login = Login.find(params[:id])
-    first_name = login['first_name'].to_s
-    last_name = login['last_name'].to_s
-    auth_token = 'da1791b6cfad9b6e800c6f0f0fcbd6ee'
-    api_context = 'crmapi'
-    xml_data = '<Leads><row no="1"><FL val="Lead Source">Web Download</FL><FL val="Company">Your Company</FL><FL val="First Name">'+first_name+'</FL><FL val="Last Name">'+last_name+'</FL></row></Leads>'
-    response = HTTParty.post(
-      'https://crm.zoho.com/crm/private/xml/Leads/insertRecords',
-      body: {
-        newFormat: '1',
-        authtoken: auth_token,
-        scope: api_context,
-        xmlData: xml_data
-      }
-    )
-    render xml: response
-  end
-
   # GET /logins/new
   # GET /logins/new.json
   def new
@@ -39,21 +17,30 @@ class LoginsController < ApplicationController
   # POST /logins
   # POST /logins.json
   def create
-    @login = Login.new(params[:login])
+    @login = Login.create(login_params)
+    login = Login.find_by(login_params)
+    first_name = login['first_name'].to_s
+    last_name = login['last_name'].to_s
+    auth_token = 'da1791b6cfad9b6e800c6f0f0fcbd6ee'
+    api_context = 'crmapi'
+    xml_data = '<Leads><row no="1"><FL val="Lead Source">Web Download</FL><FL val="Company">Your Company</FL><FL val="First Name">'+first_name+'</FL><FL val="Last Name">'+last_name+'</FL></row></Leads>'
+    response = HTTParty.post(
+      'https://crm.zoho.com/crm/private/xml/Leads/insertRecords',
+      body: {
+        newFormat: '1',
+        authtoken: auth_token,
+        scope: api_context,
+        xmlData: xml_data
+      }
+    )
+    render xml: response
     
-    respond_to do |format|
-    if @login.save
-    
-        format.html { redirect_to @login, notice: 'Login was successfully created.' }
-        format.json { render json: @login, status: :created, location: @login }
-
-      else
-        format.html { render action: "new" }
-        format.json { render json: @login.errors, status: :unprocessable_entity }
-      end
-
-    end
   end
-
+ def login_params
+      # It's mandatory to specify the nested attributes that should be whitelisted.
+      # If you use `permit` with just the key that points to the nested attributes hash,
+      # it will return an empty hash.
+      params.require(:login).permit(:first_name , :last_name)
+  end
  
 end
